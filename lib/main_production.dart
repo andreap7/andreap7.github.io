42634d8223ca0fbc3f29ba42cf52265238cd307a
Andrea <61591490+andreap7@users.noreply.github.com>
2023-03-03 19:28:22 +0100

    revert to old portfolio

--- lib/main_production.dart
+++
@@ -1,6 +0,0 @@
-import 'package:portfolio/app/app.dart';
-import 'package:portfolio/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
--- lib/portfolio/portfolio.dart
+++
@@ -1,1 +0,0 @@
-export 'view/portfolio_view.dart';
